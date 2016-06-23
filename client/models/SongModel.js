4a415f56f86b5b959693a883f97d7558a312cc88
daredia <shehzad.daredia@gmail.com>
2016-06-23 16:32:52 -0700

    Add playcount to library entry view

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,15 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
+  initialize: function() {
+    this.set('playCount', 0);
+  },
+
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+    var playCount = this.get('playCount');
+    this.set('playCount', playCount + 1);
   },
 
   enqueue: function() {
--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -3,16 +3,24 @@ var LibraryEntryView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td><%= playCount %></td>'),
 
   events: {
     'click': function() {
       this.model.enqueue();
-    }
+    },
+  },
+
+  initialize: function() {
+    this.model.on('change:playCount', function() {
+      this.render();
+    }, this);
   },
 
   render: function() {
-    return this.$el.html(this.template(this.model.attributes));
+    // return this.$el.html(this.template(this.model));
+    var playCount = this.model.get('playCount') || 0;
+    return this.$el.html(this.template( _.extend( this.model.attributes, {'playCount': playCount } ) ) );
   }
 
 });
